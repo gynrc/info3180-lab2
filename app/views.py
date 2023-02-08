1ce1e1c8eac4fea415c76149e53c6d8c2090c737
Condoleezza Gaynor <condeleezagaynor@gmail.com>
2023-02-08 10:47:30 -0500

    uploaded Lab 2 code

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+import datetime
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
@@ -11,12 +12,15 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
-
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Condoleezza Gaynor")
 
+@app.route('/profile')
+def profile():
+    """Render website's profile page."""
+    return render_template('profile.html', date=format_date_joined())
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -45,3 +49,9 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+@app.template_filter('date')
+def format_date_joined():
+    """Date function"""
+    date_joined = datetime.date(2023,2,7) #user joins in February 
+    return date_joined.strftime("%B, %Y")
